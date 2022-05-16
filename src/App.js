102a58353af3de6d0d3344c2db6b2fb6116205c3
shadzijusu1 <shadzijusu1@etf.unsa.ba>
2022-05-16 20:02:03 +0200

    Navigation between different pages added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <Router>
+    <MainNavigation/>
+      <Routes>
+      <Route path="/" element={<AllMeetups />}>
+      </Route>
+      </Routes>
+      <Routes>
+      <Route path="/new-meetup" element={<NewMeetup />}>
+      </Route>
+      </Routes>
+      <Routes>
+      <Route path="/favorites" element={<Favorites />}>
+      </Route>
+      </Routes>
+    </Router>
     </div>
+
   );
 }
 
