1902c734ad3796f83736cf4f5ee0878a9eda9ca4
shadzijusu1 <shadzijusu1@etf.unsa.ba>
2022-05-17 13:12:55 +0200

    Implemented adding favorite meetups and removing them

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
-import {BrowserRouter} from 'react-router-dom'
+import { FavoritesContextProvider } from "./store/favorites-context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<BrowserRouter><App /></BrowserRouter>);
+root.render(
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>
+);
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,61 +1,46 @@
 import MeetupList from "../components/meetups/MeetupList";
-import {useEffect, useState} from 'react'
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "First meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "Second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+import { useEffect, useState } from "react";
+
 function AllMeetupsPage() {
-  const [isloading, setIsLoading] = useState(true)
-  const [meetupData, setMeetupData] = useState([])
+  const [isloading, setIsLoading] = useState(true);
+  const [meetupData, setMeetupData] = useState([]);
   useEffect(() => {
-    setIsLoading(true)
-    fetch("https://meetups-react-1f7be-default-rtdb.firebaseio.com/meetups.json")
-    .then(response => {
-      return response.json()  
-    }).then(data => {
-      const meetups = [];
+    setIsLoading(true);
+    fetch(
+      "https://meetups-react-1f7be-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
 
         for (const key in data) {
           const meetup = {
             id: key,
-            ...data[key]
+            ...data[key],
           };
 
           meetups.push(meetup);
-      setIsLoading(false)
-      setMeetupData(meetups)
-    }
-  })
-  }, [])
- 
-  if(isloading) {
-    return <section>
-      <p>Loading...</p>
-    </section>
-  }
-  else {
-  return (
-    <section>
-      <h1>All Meetups</h1>
-      <MeetupList meetups={meetupData}></MeetupList>
-    </section>
-  );
+          setIsLoading(false);
+          setMeetupData(meetups);
+        }
+      });
+  }, []);
+
+  if (isloading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <h1>All Meetups</h1>
+        <MeetupList meetups={meetupData}></MeetupList>
+      </section>
+    );
   }
 }
 
